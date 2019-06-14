49f1fdb74d725cbf74cf905cbcd5cfab1a2d85a2
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint
    - add gitignore

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const tape = require('tape')
 const jsonist = require('jsonist')
 
-const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
+const PORT = process.env.PORT = process.env.PORT || require('get-port-sync')()
 const server = require('./server')
 
 const urlBase = `http://localhost:${PORT}`
@@ -11,6 +11,16 @@ tape('should respond hello', (t) => {
     if (err) t.error(err)
 
     t.equal(body.msg, 'hello')
+    t.end()
+  })
+})
+
+tape('should respond user-agent', (t) => {
+  const opts = { headers: { 'User-Agent': 'tape' } }
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.ua, 'tape')
     t.end()
   })
 })
